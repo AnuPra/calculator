de609d781f6f0f8fe72d3b0451a3d73d997faf88
AnuPra <40498870+AnuPra@users.noreply.github.com>
2018-11-23 19:38:56 -0600

    Added maven plugins and Improved logging.

--- src/main/java/demo/calculator/LoggerConfig.java
+++ src/main/java/demo/calculator/LoggerConfig.java
@@ -1,48 +1,42 @@
 package demo.calculator;
 
 import org.apache.logging.log4j.*;
-import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.core.config.Configurator;
 
+/**
+ * To set log levels across root. Expects 1 argument. 
+ *  "0" : ALL
+ *  "1" : ERROR
+ *  "3" : DEBUG
+ *  Else INFO
+ * @param args
+ */
 public class LoggerConfig {
 
 	private final static org.apache.logging.log4j.Logger LOG = LogManager.getRootLogger();
 	
 	// Sets log levels
 	public void setLogLevel(String level) {
+		LOG.log(Level.TRACE, "Entering setLogLevel()");
+		
 		Level newLevel;
 		
 		switch(level.trim()) {
 			case "0": newLevel = Level.ALL; break;
 			case "1": newLevel = Level.ERROR; break;
+			case "2": newLevel = Level.INFO; break;
 			case "3": newLevel = Level.DEBUG; break;
+			case "4": newLevel = Level.TRACE; break;
 			default: newLevel = Level.INFO; break;
 		}
 		
 		 Configurator.setRootLevel(newLevel);
 
-		 LOG.log(Level.ALL,"This is for all messages");
+		 /*LOG.log(Level.ALL,"This is for all messages");
 		 LOG.log(Level.ERROR,"This is for error messages");
 		 LOG.log(Level.DEBUG,"This is for debug messages");
 		 LOG.log(Level.INFO,"This is for info messages");
+		 LOG.log(Level.TRACE,"This is for trace messages");*/
 		 
-	}
-	
-	/**
-	 * To set log levels across root. Expects 1 argument. 
-	 *  "0" : ALL
-	 *  "1" : ERROR
-	 *  "3" : DEBUG
-	 *  Else INFO
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		if (args.length <=0  || args[0].trim().isEmpty()) {
-			LOG.log(Level.ERROR, "Missing expression");
-			return;
-		}
-		
-		LoggerConfig obj = new LoggerConfig();
-		obj.setLogLevel(args[0]);
 	}
 }
